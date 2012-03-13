12c0683f6fe765f42e7d8fc083b4f05d5bd78069
misha <misha.nesterenko@gmail.com>
2012-03-14 01:49:37 +0200

    fixed des algo

--- des/src/main/java/mishanesterenko/bpid/des/DES.java
+++ des/src/main/java/mishanesterenko/bpid/des/DES.java
@@ -232,10 +232,8 @@ public class DES {
             block = applyPermutation(block, IP);
             for (int j = 0; j < 16; ++j) {
                 applyFeistel(block, cachedKeys.get(j));
-                System.out.println(block);
             }
             block = applyPermutation(block, EP);
-            System.out.println("EP:" + block);
             copyBits(dataBits, block, i, 0, BLOCK_SIZE);
         }
 
@@ -256,24 +254,25 @@ public class DES {
         BitSet dataBits = BitSet.valueOf(data);
         for (int i = 0; i < dataBits.length(); i += BLOCK_SIZE) {
             BitSet block = dataBits.get(i, i + BLOCK_SIZE);
-            block = applyPermutation(block, EP);
-            System.out.println("\t" + block);
+            block = applyPermutation(block, IP);
             block = applyPermutation(block, DECRYPT_PERMUTATION);
             for (int j = 15; j >= 0; --j) {
                 applyFeistel(block, cachedKeys.get(j));
-                System.out.println("\t" + block);
             }
-            block = applyPermutation(block, IP);
+            block = applyPermutation(block, DECRYPT_PERMUTATION);
+            block = applyPermutation(block, EP);
             copyBits(dataBits, block, i, 0, BLOCK_SIZE);
         }
 
         return dataBits.toByteArray();
     }
 
-//    public static void main(String[] args) {
-//        byte[] arr = EP;
-//        for (int i = 0; i < arr.length; ++i) {
-//            System.out.print(arr[i] - 1 + ", ");
-//        }
-//    }
+    private static String toStringBitSet(final BitSet v, final int len) {
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; ++i) {
+            sb.append(v.get(i) ? "1" : "0");
+        }
+        return sb.toString();
+    }
+
 }
