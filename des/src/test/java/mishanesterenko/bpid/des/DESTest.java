db63ee079c98adfc804f50f1fef1770e6a7f9eda
misha <misha.nesterenko@gmail.com>
2012-03-14 02:38:00 +0200

    added random tests

--- des/src/test/java/mishanesterenko/bpid/des/DESTest.java
+++ des/src/test/java/mishanesterenko/bpid/des/DESTest.java
@@ -3,6 +3,8 @@ package mishanesterenko.bpid.des;
 import static org.junit.Assert.assertArrayEquals;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.Random;
 
 import org.junit.Test;
 
@@ -12,13 +14,29 @@ import org.junit.Test;
  */
 public class DESTest {
     @Test
-    public void testCrypt() throws UnsupportedEncodingException {
-        byte[] srcData = new byte[] {0, 1, 0, 0, 0, 0, 0, 0};
-        byte[] data = new byte[srcData.length]; System.arraycopy(srcData, 0, data, 0, srcData.length);
+    public void testCryptDecrypt() throws UnsupportedEncodingException {
+        Random r = new Random();
         byte[] key = {-1, -1, -1, -1, -1, -1, -1, -1};
-        DES.crypt(data, key);
-        DES.decrypt(data, key);
 
-        assertArrayEquals(srcData, data);
+        for (int i = 0; i < 10000000; ++i) {
+            byte[] srcData = new byte[8 * (r.nextInt(100) + 1)];
+            byte[] data = new byte[srcData.length];
+
+            r.nextBytes(srcData); System.arraycopy(srcData, 0, data, 0, srcData.length);
+            r.nextBytes(key);
+
+            System.out.print("key: " + Arrays.toString(key) + " src: " + Arrays.toString(srcData));
+
+            DES.crypt(data, key);
+            DES.decrypt(data, key);
+
+            try {
+                assertArrayEquals(srcData, data);
+                System.out.println(" ok!");
+            } catch (AssertionError e) {
+                System.out.println(" fail!");
+                throw e;
+            }
+        }
     }
 }
