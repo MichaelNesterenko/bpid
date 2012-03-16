e48bc3bdbf21c0368b46b4bdb923e8fb137f1011
misha <misha.nesterenko@gmail.com>
2012-03-16 02:29:51 +0200

    continue rsa. almost done

--- rsa/src/main/java/mishanesterenko/bpid/rsa/Rsa.java
+++ rsa/src/main/java/mishanesterenko/bpid/rsa/Rsa.java
@@ -1,23 +1,127 @@
 package mishanesterenko.bpid.rsa;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.util.Random;
 
 /**
  * @author Michael Nesterenko
  *
  */
 public class Rsa {
+    private final static BigInteger TWO = BigInteger.valueOf(2);
+
+    public final static int PERSIST_RADIX = 10;
+
     public static BigInteger generateRandomPrime(int bitLength) {
-        return new BigInteger("0");
+        Random rnd = new Random();
+        BigInteger num;
+        do {
+            num = BigInteger.probablePrime(bitLength, rnd);
+        } while (!isPrime(num));
+        return num;
     }
 
-    public static BigInteger pingalaHanduPower(final BigInteger base, final BigInteger deg) {
+    public static boolean isPrime(final BigInteger num) {
+        BigInteger cv = pingalaHanduPowerMod(TWO, num.add(BigInteger.ONE.negate()), num);
+
+        return BigInteger.ONE.equals(cv);
+    }
+
+    public static BigInteger pingalaHanduPowerMod(final BigInteger base, final BigInteger deg, final BigInteger mod) {
         BigInteger cv = base;
         for (int i = deg.bitLength() - 2; i >= 0; --i) {
             boolean op = deg.testBit(i);
-            cv = op ? cv.multiply(cv).multiply(base) : cv.multiply(cv);
+            cv = cv.multiply(cv);
+            cv = mod != null ? cv.mod(mod) : cv;
+            cv = op ? (mod != null ? cv.multiply(base).mod(mod) : cv.multiply(base)) : cv;
         }
 
         return cv;
     }
+
+    public static BigInteger generatePublicKey(final BigInteger p, final BigInteger q) {
+        BigInteger phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        BigInteger key = p.multiply(q);
+        int keyLength = key.bitLength() / 2;
+        BigInteger e;
+        do {
+            e = generateRandomPrime(keyLength == 0 ? 1 : keyLength);
+        } while (e.compareTo(phi) >= 0 || !e.gcd(phi).equals(BigInteger.ONE));
+        return e;
+    }
+
+    public static BigInteger generatePrivateKey(final BigInteger e, final BigInteger p, final BigInteger q) {
+        BigInteger phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        for (long k = 1; k < Long.MAX_VALUE; ++k) {
+            BigInteger up = BigInteger.ONE.add(BigInteger.valueOf(1).multiply(phi));
+            BigInteger rem = up.mod(e);
+            if (rem.equals(BigInteger.ZERO)) {
+                return up.divide(e);
+            }
+        }
+        throw new IllegalStateException("Can not create private key.");
+    }
+
+    private static void mayRsaWork(final byte[] data, final BigInteger n) {
+        int bitLength = n.bitLength();
+        if ((data.length * 8) % ((bitLength / 8) * 8) != 0) {
+            throw new IllegalArgumentException("Data size (" + data.length * 8 + ") is not multiple of key length (" + bitLength + " )");
+        }
+    }
+
+    public static void crypt(final byte[] data, final BigInteger e, final BigInteger n) {
+        mayRsaWork(data, n);
+
+        int blockSize = n.bitLength() / 8;
+        int dataSize = data.length;
+        byte[] blockData = new byte[blockSize];
+        for (int i = 0; i < dataSize; i += blockSize) {
+            System.arraycopy(data, i, blockData, 0, blockSize);
+            BigInteger block = new BigInteger(1, blockData);
+            block = pingalaHanduPowerMod(block, e, n);
+            byte[] cryptedData = block.toByteArray();
+            System.arraycopy(cryptedData, 0, data, i, blockSize);
+        }
+    }
+
+    public static void decrypt(final byte[] data, final BigInteger d, final BigInteger n) {
+        mayRsaWork(data, n);
+        crypt(data, d, n);
+    }
+
+    private static void persistKeyPair(final BufferedWriter s, final BigInteger e, final BigInteger n) throws IOException {
+        s.write(e.toString(PERSIST_RADIX));
+        s.write("\n");
+        s.write(n.toString(PERSIST_RADIX));
+    }
+
+    public static void persistPublicKey(final BufferedWriter s, final BigInteger e, final BigInteger n) throws IOException {
+        persistKeyPair(s, e, n);
+    }
+
+    public static void persistPrivateKey(final BufferedWriter s, final BigInteger d, final BigInteger n) throws IOException {
+        persistKeyPair(s, d, n);
+    }
+
+    private static BigInteger[] loadKeyPair(final BufferedReader br) throws IOException {
+        String ed = br.readLine();
+        String n = br.readLine();
+
+        BigInteger res[] = new BigInteger[2];
+        res[0] = new BigInteger(ed, PERSIST_RADIX);
+        res[1] = new BigInteger(n, PERSIST_RADIX);
+        return res;
+    }
+
+    public static BigInteger[] loadPublicPair(final BufferedReader br) throws IOException {
+        return loadKeyPair(br);
+    }
+
+    public static BigInteger[] loadPrivatePair(final BufferedReader br) throws IOException {
+        return loadKeyPair(br);
+    }
+
 }
--- rsa/src/test/java/mishanesterenko/bpid/rsa/RsaTest.java
+++ rsa/src/test/java/mishanesterenko/bpid/rsa/RsaTest.java
@@ -1,8 +1,17 @@
 package mishanesterenko.bpid.rsa;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.util.Random;
 
 import org.junit.Test;
 
@@ -12,13 +21,8 @@ import org.junit.Test;
  */
 public class RsaTest {
     @Test
-    public void testPrime() {
-        System.out.println(Rsa.generateRandomPrime(1024));
-    }
-
-    @Test
     public void testPingaluHinduPower() {
-        final int maxIterations = 1000;
+        final int maxIterations = 100;
 
         long start = System.currentTimeMillis();
         long dummy = 0;
@@ -33,14 +37,14 @@ public class RsaTest {
 
         start = System.currentTimeMillis();
         for (int i = 2; i <= maxIterations; ++i) {
-            if (i % 100 == 0) {
+            if (i % 10 == 0) {
                 end = System.currentTimeMillis();
-                System.out.println("Average time per one power is: " + ((double)(start - end)) / 100);
+                System.out.println("Average time per one power is: " + ((double)(start - end)) / 10);
             }
             for (int j = 1; j <= maxIterations; ++j) {
                 BigInteger base = BigInteger.valueOf(i);
                 BigInteger deg = BigInteger.valueOf(j);
-                BigInteger pingalaHindu = Rsa.pingalaHanduPower(base, deg);
+                BigInteger pingalaHindu = Rsa.pingalaHanduPowerMod(base, deg, null);
 
                 BigInteger plain = BigInteger.valueOf(i);
                 BigInteger plainBase = BigInteger.valueOf(i);
@@ -51,5 +55,93 @@ public class RsaTest {
                 assertEquals(i + "^" + j, plain, pingalaHindu);
             }
         }
+    }
+
+    @Test
+    public void testIsPrime() {
+        BigInteger num = new BigInteger("127108435736657059435269177623415244157933295178004463647952130224955945070962826236530834080865939342537515852129288392436922163509707156507151966268454895629991545164838864260139856947329238003048672417162072429488042283379281437514670693419290392197529869131163708486564371678503364652796819439743454964233");
+        assertTrue(Rsa.isPrime(num));
+    }
+
+    @Test
+    public void testPrimeGeneration() {
+        final int keyLength= 1024;
+        BigInteger prime = Rsa.generateRandomPrime(keyLength);
+        assertEquals(keyLength, prime.bitLength());
+        System.out.println(prime);
+    }
+
+    @Test
+    public void testGeneratePublicKey() {
+        BigInteger p = Rsa.generateRandomPrime(1024);
+        BigInteger q = Rsa.generateRandomPrime(1024);
+        System.out.println(Rsa.generatePublicKey(p, q));
+    }
+
+    @Test
+    public void testGeneratePrivateKey() {
+        BigInteger p = Rsa.generateRandomPrime(1024);
+        BigInteger q = Rsa.generateRandomPrime(1024);
+        BigInteger e = Rsa.generatePublicKey(p, q);
+        BigInteger d = Rsa.generatePrivateKey(e, p, q);
+        System.out.println(d);
+    }
+
+    @Test
+    public void testKeyPersist() throws IOException {
+        final int keyLength = 8;
+        BigInteger p = Rsa.generateRandomPrime(keyLength / 2);
+        BigInteger q = Rsa.generateRandomPrime(keyLength / 2);
+        while (p.multiply(q).bitLength() != keyLength) {
+            p = Rsa.generateRandomPrime(keyLength / 2);
+            q = Rsa.generateRandomPrime(keyLength / 2);
+        }
+        BigInteger e = Rsa.generatePublicKey(p, q);
+        BigInteger d = Rsa.generatePrivateKey(e, p, q);
+        BigInteger n = p.multiply(q);
+
+        BufferedWriter fw = new BufferedWriter(new FileWriter("key.pub"));
+        Rsa.persistPublicKey(fw, e, n);
+        fw.close();
+        fw = new BufferedWriter(new FileWriter("key"));
+        Rsa.persistPrivateKey(fw, d, n);
+        fw.close();
+
+        BigInteger[] keys = Rsa.loadPublicPair(new BufferedReader(new FileReader("key.pub")));
+        BigInteger eLoaded = keys[0];
+        BigInteger publicN = keys[1];
+
+        keys = Rsa.loadPublicPair(new BufferedReader(new FileReader("key.pub")));
+        BigInteger dLoaded = keys[0];
+        BigInteger privateN = keys[1];
+
+        assertTrue("Modulos should be equal", publicN.equals(privateN));
+        assertEquals("Public key parts should be equal", e, eLoaded);
+        assertEquals("Private key parts should be equal", e, dLoaded);
+    }
+
+    @Test
+    public void testRsa() throws FileNotFoundException, IOException {
+        final int keyLength = 1024;
+        byte[] srcData = new byte[2 * keyLength / 8];
+        byte[] data = new byte[srcData.length];
+
+        new Random().nextBytes(srcData);
+        System.arraycopy(srcData, 0, data, 0, data.length);
+
+        BigInteger[] keys = Rsa.loadPublicPair(new BufferedReader(new FileReader("key.pub")));
+        BigInteger e = keys[0];
+        BigInteger publicN = keys[1];
+
+        keys = Rsa.loadPrivatePair(new BufferedReader(new FileReader("key")));
+        BigInteger d = keys[0];
+        BigInteger privateN = keys[1];
+
+        assertTrue("Modulos should be equal", publicN.equals(privateN));
+
+        Rsa.crypt(data, e, publicN);
+        Rsa.decrypt(data, d, publicN);
+
+        assertArrayEquals(srcData, data);
     }
 }
